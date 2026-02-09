e0a5a8381f1ac7bc44e201f8d1665bb504fc445e
Rokhydiockou <rokhydiockou83@gmail.com>
2026-02-09 23:48:54 +0000

    Add files via upload

---
+++ cv1.py
@@ -0,0 +1,117 @@
+import streamlit as st
+with st. sidebar:
+    with st.container(border=True):
+       st. header("ACCEUIL💡")
+    
+       st. image("ro.jpeg",width=150)
+       st. divider()
+        
+       st. write("INFORMATION PERSONNELLE📑")
+       st. header("""
+      
+       -*DIOCKOU
+       
+       -*ROKHY
+       
+       -*781646598
+       
+       -*EMAIL📧:rokhydiockou83@gmail.com
+       
+       -*SICAP AMITIE/zoneB
+       """)
+       st. divider()
+       st. write ("DIPLOME🎓")
+       st. header("""
+       
+       -**2025_2026**:BTS en GEOMATIQUE
+       
+       -**2024_2025**:LICENCE en GEOGRAPHIE
+       
+       -**2021_2022**:BACCALAUREAT en série L2
+       """)
+       
+       st. divider()
+
+with st. container(border=True):
+    st. title (" BIENVENUE💎!")
+    st. divider ()
+with st. container (border=True):
+    st. header("PROFIL☺")
+st. markdown ("INGENIEUR EN GEOMATIQUE: mes spécialités c'est les SYSTEMES D'INFORMATIONS GEOGRAPHIQUES, plus particuliérement en CARTOGRAPHIE, BASE DE DONNEE, PROGRAMMATION ")
+st. divider()
+with st. container(border=True):
+    st. header("PROJET ACADEMIQUE📚")
+st. markdown("""
+
+-** réalisation d'une carte de situation de la région de KAOLACK
+
+-** réalisation d'une de carte des types de sols de la région de SAINT LOUIS
+
+-** réalisation d'une carte administrative du SENEGAL
+
+-** réalisation des diagrammes sur excel ( circulaire, barre, courbe )
+
+-** collecte et traitement des données du Centre de Formation CEDT G15.
+
+-** collecte et traitement des données de MASSALIKU DJINAL
+
+-** collecte et traitement des données de colobane , fasse , gueule tapé
+
+-** création d'application calculatrice avec python
+
+-** création d'application de bibliothéque avec python
+
+-** réalistion d'un plan de maison ( rez de chaussée , terrasse ,facade ,masse , coupe )
+
+-** la numération du quartier de ZONE B
+
+-** création d'une base de donnée dans QGIS et ARCGIS
+
+-** faire les données MNT de la région de kaolack
+
+-** création d'une machine virtuelle dans un ordinateur
+
+
+""")
+
+st. divider()
+
+with st. container(border=True):
+ 
+  st. header("MES COMPETENCES📝")
+st. markdown("""
+MAITRISE DES SIG : QGIS,ARGIS ( collect ,traitement de données, réalisation des cartes( thématique, situation,administrative)
+
+PROGRAMMATION en PYTHON
+
+DESSIN PLAN: réalisation des plan ( rez de chaussée , etage, masse, terrasse, coupe, facade)
+
+BASE DE DONNEES :
+Numérisation , Géoreférencement, 
+
+MAITRISE : SUITE BUREAUTIQUE (word,excel , power point)
+
+""")  
+
+st. divider()
+
+with st. container (border=True):
+
+ st. header("MES EXPERIENCES⏳")
+
+st. markdown("""
+-** COSTUMISATION: des sacs à mains , cahier , des chaussures
+
+-** PATISSERIE: fataya , gateau, pizza,sucré,salé
+""")
+
+st. divider()
+with st. container (border=True):
+    st. header("ME CONTACTER☎")
+
+
+st. divider()
+
+st. success(" MERCI DE VOTRE VISITE✨👍 !")
+           
+                                                     
